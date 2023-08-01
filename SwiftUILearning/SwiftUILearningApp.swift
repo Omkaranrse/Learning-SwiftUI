45abd628815af369b49ec01cebd45929f5ab25f9
omkar <omkaranarse1906@gmail.com>
2023-08-01 22:16:41 +0530

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Omkar Anarse on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
